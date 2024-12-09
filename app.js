90bcc0b353d863ddd643bc3a190ce5da6f4326aa
VSocks <marvinpsilva2003@gmail.com>
2024-12-08 22:17:32 -0300

    novas funções do menu movidas para arquivo dedicado separado de app.js

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const cadastroClienteRoutes = require('./routes/cadastroCliente'); // Alteraçã
 const cadastroFuncionarioRoutes = require('./routes/cadastroFuncionario');
 const cadastroLivroRoutes = require('./routes/cadastroLivro');
 const loginRoutes = require('./routes/login');
+const menuRoutes = require('./routes/menu');  // Importando as rotas do menu
 const db = require('./database');
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Rota inicial
@@ -25,56 +27,7 @@ app.use('/cadastroCliente', cadastroClienteRoutes); // Alteração da branch fea
 app.use('/cadastroFuncionario', cadastroFuncionarioRoutes);
 app.use('/cadastroLivro', cadastroLivroRoutes);
 app.use('/login', loginRoutes);
-
-
-
-// Rota para obter os dados de Funcionários
-app.get('/dados/funcionarios', (req, res) => {
-  const sql = 'SELECT * FROM Funcionarios';
-  db.query(sql, (err, results) => {
-    if (err) return res.status(500).json({ error: 'Erro no servidor' });
-    res.json(results);
-  });
-});
-
-// Rota para obter os dados de Clientes
-app.get('/dados/clientes', (req, res) => {
-  const sql = 'SELECT * FROM Clientes';
-  db.query(sql, (err, results) => {
-    if (err) return res.status(500).json({ error: 'Erro no servidor' });
-    res.json(results);
-  });
-});
-
-// Rota para obter os dados de Livros
-app.get('/dados/livros', (req, res) => {
-  const sql = 'SELECT * FROM Livros';
-  db.query(sql, (err, results) => {
-    if (err) return res.status(500).json({ error: 'Erro no servidor' });
-    res.json(results);
-  });
-});
-
-// Rota para obter os dados de Empréstimos
-app.get('/dados/emprestimos', (req, res) => {
-  const sql = 'SELECT * FROM Emprestimos';
-  db.query(sql, (err, results) => {
-    if (err) return res.status(500).json({ error: 'Erro no servidor' });
-    res.json(results);
-  });
-});
-
-// Rota para obter os dados de Reservas
-app.get('/dados/reservas', (req, res) => {
-  const sql = 'SELECT * FROM Reservas'; // Supondo que a tabela Reservas já exista no banco
-  db.query(sql, (err, results) => {
-    if (err) return res.status(500).json({ error: 'Erro no servidor' });
-    res.json(results);
-  });
-});
-
-
-
+app.use(menuRoutes);
 
 // Iniciar o servidor
 const PORT = 3000;
